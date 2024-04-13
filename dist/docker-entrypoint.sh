31cd585cab8f06d7f2ba021dadfe837a7c4967b2
maany <imptodefeat@gmail.com>
2024-04-13 14:10:28 +0200

    remove httpd in prod containers

--- dist/docker-entrypoint.sh
+++ dist/docker-entrypoint.sh
@@ -6,23 +6,7 @@ log() {
 log "Generating ecosystem.config.js"
 j2 dist/ecosystem.config.js.j2 > /opt/dadai/app/ecosystem.config.cjs
 echo "=================== /opt/dadai/app/ecosystem.config.j2 ==================="
-cat /opt/dadai/app/ecosystem.config.j2
-
-
-log "Building Apache configuration files."
-j2 dist/httpd.conf.j2 | sed '/^\s*$/d' > /etc/httpd/conf/httpd.conf
-echo "=================== /etc/httpd/conf/httpd.conf ========================"
-cat /etc/httpd/conf/httpd.conf
-echo ""
-
-log "Building Websat Planckster Apache configuration files."
-j2 dist/dad.conf.j2 | sed '/^\s*$/d' > /etc/httpd/conf.d/dad.conf
-echo "=================== /etc/httpd/conf/conf.d/dad.conf ========================"
-cat /etc/httpd/conf.d/dad.conf
-echo ""
-
-log "Removing default Apache SSL configuration files."
-rm -f /etc/httpd/conf.d/ssl.conf
+cat /opt/dadai/app/ecosystem.config.cjs
 
 log "Building NextJS application"
 cd /opt/dadai/app
@@ -31,8 +15,23 @@ npm run build
 log "Starting pm2"
 npx pm2 start ecosystem.config.cjs
 
-log "Starting Apache"
-httpd
+# log "Building Apache configuration files."
+# j2 dist/httpd.conf.j2 | sed '/^\s*$/d' > /etc/httpd/conf/httpd.conf
+# echo "=================== /etc/httpd/conf/httpd.conf ========================"
+# cat /etc/httpd/conf/httpd.conf
+# echo ""
+
+# log "Building Websat Planckster Apache configuration files."
+# j2 dist/dad.conf.j2 | sed '/^\s*$/d' > /etc/httpd/conf.d/dad.conf
+# echo "=================== /etc/httpd/conf/conf.d/dad.conf ========================"
+# cat /etc/httpd/conf.d/dad.conf
+# echo ""
+
+# log "Removing default Apache SSL configuration files."
+# rm -f /etc/httpd/conf.d/ssl.conf
+
+# log "Starting Apache"
+# httpd
 
 log "Starting pm2 logs"
 npx pm2 logs 
