4c82745d41e8a3e9ca19cccc93449b564d24716b
maany <imptodefeat@gmail.com>
2024-08-06 22:29:44 +0200

    filerepo: add server file repo and implement upload feature task-179

--- src/lib/infrastructure/server/config/ioc/server-ioc-symbols.ts
+++ src/lib/infrastructure/server/config/ioc/server-ioc-symbols.ts
@@ -23,3 +23,8 @@ export const GATEWAYS = {
     AUTH_GATEWAY: Symbol.for('AUTH_GATEWAY'),
     AGENT_GATEWAY: Symbol.for('AGENT_GATEWAY'),
 }
+
+export const REPOSITORY = {
+    KERNEL_FILE_REPOSITORY: Symbol.for('FILE_REPOSITORY'),
+}
+
