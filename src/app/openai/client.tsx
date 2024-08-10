6e318e1581c9738760c3b6e60f2ba8e1f6141ce8
maany <imptodefeat@gmail.com>
2024-08-10 14:03:11 +0200

    openai: add react components for debugging and testing

---
+++ src/app/openai/client.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export const OpenAIClientComponent = () => {
+    return (
+        <div>
+            This is the client side of the OpenAI client.
+
+        </div>
+    )
+};
---
+++ src/app/openai/page.tsx
@@ -0,0 +1,33 @@
+import serverContainer from "~/lib/infrastructure/server/config/ioc/server-container";
+import { OpenAIClientComponent } from "./client";
+import type OpenAIRemoteStorageElement from "~/lib/infrastructure/server/repository/openai-remote-storage-element";
+import { OPENAI, UTILS } from "~/lib/infrastructure/server/config/ioc/server-ioc-symbols";
+import fs from "fs";
+import { LocalFile } from "~/lib/core/entity/file";
+import { Logger } from "pino";
+
+export default async function OpenAIServerComponent() {
+    const OpenAIRSE  = serverContainer.get<OpenAIRemoteStorageElement>(OPENAI.OPENAI_REMOTE_STORAGE_ELEMENT);
+    const loggerFactory = serverContainer.get<(module: string) => Logger>(UTILS.LOGGER_FACTORY);
+    const logger = loggerFactory("OpenAIServerComponent");
+    const file: LocalFile = {
+        type: "local",
+        path: "hello.txt"
+    }
+    fs.writeFileSync(file.path, "Hello, World!");
+
+    const dto = await OpenAIRSE.downloadFile({
+        type: "remote",
+        provider: "openai",
+        path: "file-jMpOuxGJXgqddGKDaWnhlW3t"
+    });
+    logger.info({ dto }, `Uploaded file ${file.path} to OpenAI.`);
+    return (
+        <div>
+            <h1>OpenAI Server Component</h1>
+            
+            <p>File uploaded to OpenAI</p>
+            <OpenAIClientComponent />
+        </div>
+    )
+}
