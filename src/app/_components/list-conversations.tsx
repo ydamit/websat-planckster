3b5ee1e4c9986d28e58e21a79f9021ab5c833cbf
Luis Alejandro Bordo García <bgluiszz@gmail.com>
2024-08-30 19:02:38 +0200

    try: dummy list conversations to test controller

--- src/app/_components/list-conversations.tsx
+++ src/app/_components/list-conversations.tsx
@@ -1,15 +1,36 @@
 "use client";
 import { type TConversation } from "~/lib/core/entity/kernel-models";
 import { type TListConversationsViewModel } from "~/lib/core/view-models/list-conversations-view-model";
+import { type ConversationAGGridProps, ConversationAGGrid } from "@maany_shr/rage-ui-kit"
 
 
 export function ListConversationsClientPage(viewModel: TListConversationsViewModel) {
+
   if (viewModel.status == "error") {
+
+    const errorOverlayProps = {
+      errorStatus: true,
+      overlayText: `An error occurred: ${viewModel.message}`
+    }
+
+    console.log(`An error occurred: ${viewModel.message}.\nContext of the error:\n${viewModel.context}`)
+
+    console.log(`dump of the VM: ${JSON.stringify(viewModel)}`)
+
+    const conversationAGGridProps: ConversationAGGridProps = {
+      rowData: [],
+      isLoading: false,
+      goToConversation: () => "",
+      handleNewConversation: () => "",
+      errorOverlayProps: errorOverlayProps
+    }
+      
     return (
-      <div>
-        <h1>Error</h1>
-        <p>{viewModel.message}</p>
-      </div>
+      <ConversationAGGrid {...conversationAGGridProps} />
+      //<div>
+        //<h1>Error</h1>
+        //<p>{viewModel.message}</p>
+      //</div>
     );
   }
 
