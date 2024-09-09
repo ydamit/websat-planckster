20c0091ce82975d98382e6d0be239c7b83e8f049
maany <imptodefeat@gmail.com>
2024-09-09 02:47:49 +0200

    example

--- src/app/_components/list-conversations.tsx
+++ src/app/_components/list-conversations.tsx
@@ -3,24 +3,33 @@ import { type TListConversationsViewModel } from "~/lib/core/view-models/list-co
 import { useState } from "react";
 import signalsContainer from "~/lib/infrastructure/common/signals-container";
 import type { Signal } from "~/lib/core/entity/signals";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { SIGNAL_FACTORY } from "~/lib/infrastructure/common/signals-ioc-container";
 import clientContainer from "~/lib/infrastructure/client/config/ioc/client-container";
 import { CONTROLLERS } from "~/lib/infrastructure/client/config/ioc/client-ioc-symbols";
 import type BrowserListConversationsController from "~/lib/infrastructure/client/controller/browser-list-conversations-controller";
 import type { TBrowserListConversationsControllerParameters } from "~/lib/infrastructure/client/controller/browser-list-conversations-controller";
+import { Button } from "@maany_shr/rage-ui-kit";
+import { TCreateConversationViewModel } from "~/lib/core/view-models/create-conversation-view-model";
+import BrowserCreateConversationController, { TBrowserCreateConversationControllerParameters } from "~/lib/infrastructure/client/controller/browser-create-conversation-controller";
 
 // TODO: look at one of the ralph pages
 export function ListConversationsClientPage(props: { viewModel: TListConversationsViewModel; researchContextID: number }) {
   const [listConversationsViewModel, setListConversationsViewModel] = useState<TListConversationsViewModel>(props.viewModel);
-
+  const [createConversationViewModel, setCreateConversationViewModel] = useState<TCreateConversationViewModel>({
+    status: "request",
+  } as TCreateConversationViewModel);
+  const queryClient = useQueryClient();
   const { isFetching, isLoading, isError } = useQuery<Signal<TListConversationsViewModel>>({
     queryKey: [`list-conversations#${props.researchContextID}`],
     queryFn: async () => {
       const signalFactory = signalsContainer.get<(initialValue: TListConversationsViewModel, update?: (value: TListConversationsViewModel) => void) => Signal<TListConversationsViewModel>>(SIGNAL_FACTORY.KERNEL_LIST_CONVERSATIONS);
-      const response: Signal<TListConversationsViewModel> = signalFactory({
-        status: "request",
-      }, setListConversationsViewModel);
+      const response: Signal<TListConversationsViewModel> = signalFactory(
+        {
+          status: "request",
+        },
+        setListConversationsViewModel,
+      );
       const controllerParameters: TBrowserListConversationsControllerParameters = {
         response: response,
         researchContextID: props.researchContextID,
@@ -31,19 +40,61 @@ export function ListConversationsClientPage(props: { viewModel: TListConversatio
     },
   });
 
-  if (isFetching || isLoading) {
-    return <div>Loading State of Table Component. Disable the create new conversation button and show a spinner somewhere</div>;
-  }
-  if(isError){
-    return <div>Error State of Table Component. Use a toast to show we cannot fetch data anymore.</div>
-  }
-  if (listConversationsViewModel.status === "error") {
-    return <div>Error VM: {JSON.stringify(listConversationsViewModel)}</div>;
-  } else if (listConversationsViewModel.status === "success") {
-    return <div><ul>
-      {listConversationsViewModel.conversations.map((conversation) => {
-        return <li key={conversation.id}>{conversation.title}</li>;
-      })}
-      </ul></div>;
-  }
+  const enableCreateConversation = !isFetching || !isLoading;
+
+  const mutation = useMutation({
+    mutationKey: ["create-conversation"],
+    retry: 3,
+    retryDelay: 3000,
+    onSuccess: async (data) => {
+      await queryClient.invalidateQueries({queryKey: [`list-conversations#${props.researchContextID}`]});
+    },
+    mutationFn: async (title: string) => {
+      const signalFactory = signalsContainer.get<(initialValue: TCreateConversationViewModel, update?: (value: TCreateConversationViewModel) => void) => Signal<TCreateConversationViewModel>>(SIGNAL_FACTORY.KERNEL_CREATE_CONVERSATION);
+      const response: Signal<TCreateConversationViewModel> = signalFactory(
+        {
+          status: "request",
+        } as TCreateConversationViewModel,
+        setCreateConversationViewModel,
+      );
+      const controller = clientContainer.get<BrowserCreateConversationController>(CONTROLLERS.CREATE_CONVERSATION_CONTROLLER);
+      const controllerParameters: TBrowserCreateConversationControllerParameters = {
+        response: response,
+        researchContextID: props.researchContextID,
+        title: title,
+      };
+      await controller.execute(controllerParameters);
+    },
+  });
+  const handleCreateConversation = (title: string) => {
+    console.log("Creating conversation with title: ", title);
+    mutation.mutate(title);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-between">
+      {enableCreateConversation && <CreateConversationButton onCreateConversation={handleCreateConversation} />}
+      <div>
+        {listConversationsViewModel.status === "success" && (
+          <ul>
+            {listConversationsViewModel.conversations.map((conversation) => {
+              return <li key={conversation.id}>{conversation.title}</li>;
+            })}
+          </ul>
+        )}
+        {listConversationsViewModel.status === "error" && <div>Error VM: {JSON.stringify(listConversationsViewModel)}</div>}
+      </div>
+    </div>
+  );
 }
+
+const CreateConversationButton = (props: { onCreateConversation: (title: string) => void }) => {
+  return (
+    <Button
+      label="Create New Conversation"
+      onClick={() => {
+        props.onCreateConversation("Conny Cucumber");
+      }}
+    />
+  );
+};
