55b102a4f169fba927bb74bc8cdfe571dff8cc44
Yotam <paxarchy@gmail.com>
2024-09-09 22:41:16 -0400

    add download feature to client and research context source data pages

--- src/app/[rc_id]/sources/page.tsx
+++ src/app/[rc_id]/sources/page.tsx
@@ -1,58 +1,49 @@
 import { redirect } from "next/navigation";
 import type AuthGatewayOutputPort from "~/lib/core/ports/secondary/auth-gateway-output-port";
 import serverContainer from "~/lib/infrastructure/server/config/ioc/server-container";
-import { CONTROLLERS, GATEWAYS, TRPC } from "~/lib/infrastructure/server/config/ioc/server-ioc-symbols";
-import ListSourceDataController from "~/lib/infrastructure/server/controller/list-source-data-controller";
-import type { TServerComponentAPI } from "~/lib/infrastructure/server/trpc/server-api";
+import { CONTROLLERS, GATEWAYS } from "~/lib/infrastructure/server/config/ioc/server-ioc-symbols";
+import { ListSourceDataForResearchContextClientPage } from "../../_components/list-source-data-research-context";
+import ListSourceDataController, { TListSourceDataControllerParameters } from "~/lib/infrastructure/server/controller/list-source-data-controller";
+import signalsContainer from "~/lib/infrastructure/common/signals-container";
+import { TListSourceDataViewModel } from "~/lib/core/view-models/list-source-data-view-models";
+import type { Signal } from "~/lib/core/entity/signals";
+import { SIGNAL_FACTORY } from "~/lib/infrastructure/common/signals-ioc-container";
+import { Suspense } from "react";
 
-//export default async function Home(
-    //{ params }: { params: { rc_id: number } }
-//) {
-  //const authGateway = serverContainer.get<AuthGatewayOutputPort>(GATEWAYS.AUTH_GATEWAY);
-  //const sessionDTO = await authGateway.getSession();
-  //if (!sessionDTO.success) {
-    //redirect("/auth/login");
-  //};
-  //return (
-        //<ListSourceData rc_id={params.rc_id} />
-  //);
-//}
 
-//async function ListSourceData({ rc_id }: { rc_id: number }) {
-  //const controller = serverContainer.get<ListSourceDataController>(CONTROLLERS.LIST_SOURCE_DATA_CONTROLLER);
-  
-  //const sourceDataVM = await api.kernel.sourceData.list(
-    //{
-        //researchContextID: rc_id,
-    //}
-  //)
+export default async function ListSourceDataForResearchContextServerPage(
+  { params }: { params: { rc_id: string } }
+) {
+  const authGateway = serverContainer.get<AuthGatewayOutputPort>(GATEWAYS.AUTH_GATEWAY);
+  const sessionDTO = await authGateway.getSession();
+  if (!sessionDTO.success) {
+    redirect("/auth/login");
+  }
 
-  //if (sourceDataVM.status === "error") {
-    //return <div>Error: {sourceDataVM.message}</div>;
-  //}
+  const researchContextID = parseInt(params.rc_id);
+  // Initialize the source data to show
+  const controller = serverContainer.get<ListSourceDataController>(CONTROLLERS.LIST_SOURCE_DATA_CONTROLLER);
 
-  //if (sourceDataVM.status === "success" && !sourceDataVM.sourceData) {
-    //return <div>No source data found</div>;
-  //}
+  const signalFactory = signalsContainer.get<(initialValue: TListSourceDataViewModel, update?: (value: TListSourceDataViewModel) => void) => Signal<TListSourceDataViewModel>>(SIGNAL_FACTORY.KERNEL_LIST_SOURCE_DATA);
 
-  //if (sourceDataVM.status === "success" && sourceDataVM.sourceData.length === 0) {
-    //return <div>No source data found</div>;
-  //}
+  const response: Signal<TListSourceDataViewModel> = signalFactory({
+    status: "request",
+  });
 
-  //if (sourceDataVM.status === "success") {
-  //const sourceData = sourceDataVM.sourceData;
+  const controllerParameters: TListSourceDataControllerParameters = {
+    response: response,
+  };
 
-  //// Return a simple HTML unordered list
-  //return (
-   //<ul>
-    //{sourceData.map((data, index) => (
-      //<li key={index}>
-        //{data.relativePath}
-      //</li>
-    //))}
-  //</ul>   
+  await controller.execute(controllerParameters);
 
-  //);
-
-//}
-//}
+  return (
+    <div className="flex flex-col gap-4">
+      <Suspense fallback={<div>AG GRID SKELETON...</div>}>
+        <ListSourceDataForResearchContextClientPage
+          viewModel={response.value}
+          researchContextID={researchContextID}
+        />
+      </Suspense>
+    </div>
+  );
+}
---
+++ src/app/_components/list-source-data-client.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import { SourceDataAGGrid } from "@maany_shr/rage-ui-kit";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useRef, useState } from "react";
+import { Signal } from "~/lib/core/entity/signals";
+import { TFileDownloadViewModel } from "~/lib/core/view-models/file-download-view-model";
+import { TFileUploadViewModel } from "~/lib/core/view-models/file-upload-view-model";
+import { TListSourceDataViewModel } from "~/lib/core/view-models/list-source-data-view-models";
+import clientContainer from "~/lib/infrastructure/client/config/ioc/client-container";
+import { CONTROLLERS } from "~/lib/infrastructure/client/config/ioc/client-ioc-symbols";
+import BrowserFileDownloadController, { TBrowserFileDownloadControllerParameters } from "~/lib/infrastructure/client/controller/browser-file-download-controller";
+import BrowserFileUploadController, { TBrowserFileUploadControllerParameters } from "~/lib/infrastructure/client/controller/browser-file-upload-controller";
+import BrowserListSourceDataController, { TBrowserListSourceDataControllerParameters } from "~/lib/infrastructure/client/controller/browser-list-source-data-controller";
+import signalsContainer from "~/lib/infrastructure/common/signals-container";
+import { SIGNAL_FACTORY } from "~/lib/infrastructure/common/signals-ioc-container";
+
+export function ListSourceDataForClientClientPage(props: { viewModel: TListSourceDataViewModel }) {
+
+  const [uploadSourceDataViewModel, setUploadSourceDataViewModel] = useState<TFileUploadViewModel>({
+    status: "request",
+  } as TFileUploadViewModel);
+
+  const [downloadSourceDataViewModel, setDownloadSourceDataViewModel] = useState<TFileDownloadViewModel>({
+    status: "request",
+  } as TFileDownloadViewModel);
+
+  const [listSourceDataViewModel, setListSourceDataViewModel] = useState<TListSourceDataViewModel>(props.viewModel);
+
+  const queryClient = useQueryClient();
+
+  const { isFetching, isLoading, isError } = useQuery<Signal<TListSourceDataViewModel>>({
+    queryKey: ["list-source-data"],
+    queryFn: async () => {
+      const signalFactory = signalsContainer.get<(initialValue: TListSourceDataViewModel, update?: (value: TListSourceDataViewModel) => void) => Signal<TListSourceDataViewModel>>(SIGNAL_FACTORY.KERNEL_LIST_SOURCE_DATA);
+      const response: Signal<TListSourceDataViewModel> = signalFactory(
+        {
+          status: "request",
+        },
+        setListSourceDataViewModel,
+      );
+      const controllerParameters: TBrowserListSourceDataControllerParameters = {
+        response: response,
+      };
+      const controller = clientContainer.get<BrowserListSourceDataController>(CONTROLLERS.LIST_SOURCE_DATA_CONTROLLER);
+      await controller.execute(controllerParameters);
+      return response;
+    },
+  });
+
+  const downloadMutation = useMutation({
+    mutationKey: ["download-source-data"],
+    retry: 3,
+    retryDelay: 3000,
+    mutationFn: async (params: {relativePath: string, sourceDataName: string}) => {
+      const signalFactory = signalsContainer.get<(initialValue: TFileDownloadViewModel, update?: (value: TFileDownloadViewModel) => void) => Signal<TFileDownloadViewModel>>(SIGNAL_FACTORY.KERNEL_FILE_DOWNLOAD);
+      const response: Signal<TFileDownloadViewModel> = signalFactory(
+        {
+          status: "request",
+        } as TFileDownloadViewModel,
+        setDownloadSourceDataViewModel,
+      );
+      const controllerParameters: TBrowserFileDownloadControllerParameters = {
+        response: response,
+        relativePath: params.relativePath,
+        localPath: params.sourceDataName,
+      };
+      const controller = clientContainer.get<BrowserFileDownloadController>(CONTROLLERS.KERNEL_FILE_DOWNLOAD_CONTROLLER);
+      await controller.execute(controllerParameters);
+      return response;
+    },
+  });
+
+  const uploadMutation = useMutation({
+    mutationKey: ["upload-source-data"],
+    retry: 3,
+    retryDelay: 3000,
+    onSuccess: async (data) => {
+      await queryClient.invalidateQueries({ queryKey: ["list-source-data"] });
+    },
+    mutationFn: async (file: File) => {
+      const signalFactory = signalsContainer.get<(initialValue: TFileUploadViewModel, update?: (value: TFileUploadViewModel) => void) => Signal<TFileUploadViewModel>>(SIGNAL_FACTORY.KERNEL_FILE_UPLOAD);
+      const response: Signal<TFileUploadViewModel> = signalFactory(
+        {
+          status: "request",
+        } as TFileUploadViewModel,
+        setUploadSourceDataViewModel,
+      );
+      const controllerParameters: TBrowserFileUploadControllerParameters = {
+        response: response,
+        file: file,
+      };
+      const controller = clientContainer.get<BrowserFileUploadController>(CONTROLLERS.KERNEL_FILE_UPLOAD_CONTROLLER);
+      await controller.execute(controllerParameters);
+      return response;
+    },
+  });
+
+
+  const handleDownloadSourceData = (name: string, relativePath: string) => {
+    console.log("Download source data", relativePath, name);
+    downloadMutation.mutate({relativePath: relativePath, sourceDataName: name});
+  };
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleUploadSourceData = () => {
+    console.log("Upload source data");
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    } 
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files?.[0]) {
+      const file = event.target.files[0];
+
+      if (!file) {
+        return;
+      }
+
+      uploadMutation.mutate(file);
+    }
+  }
+
+  const isUploading = uploadMutation.isPending
+
+
+  if (listSourceDataViewModel.status === "request") {
+    return (
+      <div>
+        <SourceDataAGGrid isLoading={true} isUploading={false} rowData={[]} handleDownloadSourceData={handleDownloadSourceData} handleUploadSourceData={handleUploadSourceData} />
+      </div>
+    );
+  } else if (listSourceDataViewModel.status === "success") {
+    return (
+      <div>
+        <SourceDataAGGrid isLoading={false} enableUpload={true} isUploading={isUploading} rowData={listSourceDataViewModel.sourceData} handleDownloadSourceData={handleDownloadSourceData} handleUploadSourceData={handleUploadSourceData} />
+
+        <input
+          type="file"
+          ref={fileInputRef}
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+      </div>
+    );
+  }
+}
